c38a6b63069dd728f0ffdbde7abbd77a2f258841
Pratanu Ghorui <ghoruipratanu@gmail.com>
2026-04-22 08:56:18 +0530

    modify git ignore

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+//this is for json data
+app.use(express.json({ limit: "16kb" }));
+
+//this is for url data
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  }),
+);
+
+app.use(express.static("public"));
+
+const userRouter = require("./routers/user.routes");
+// const adminRouter = require('./routers/admin.routes');
+// const paymentRouter = require('./routers/payment.routes');
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+app.use("/api/users", userRouter);
+// app.use('/api/admin', adminRouter);
+// app.use('/api/payment', paymentRouter);
+
+module.exports = { app };
